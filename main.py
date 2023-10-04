053ca45738632397f0094b33dd8ab73a44079f60
magnob5 <magnosib5@gmail.com>
2023-10-03 23:53:13 -0300

    ultimo comitt

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_cors import CORS
 from helpers.validacoes import validar_senha
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
-    return render_template('cadastro.html')
+    if request.method == 'GET':
+      return render_template('cadastro.html')
+    
+    if request.method == 'POST':
+      nomeCompleto = request.form['nomeCompleto']
+      cpf = request.form['cpf']
+      email = request.form['email']
+      telefone = request.form['telefone']
+      endereco = request.form['endereco']
+      senha = request.form['senha']
+
+      confirmarSenha = request.form['confirmarSenha']
+
+      resultado = validar_senha(senha)
+
+      if senha != confirmarSenha:
+        resultado.append('As senhas devem ser iguais')
+
+      if resultado is True:
+        return redirect('/')
+
+      else:
+        return render_template('cadastro.html', erros=resultado, nomeCompleto=nomeCompleto, cpf=cpf, telefone=telefone, endereco=endereco, email=email)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -20,15 +40,6 @@ def login():
       return render_template('login.html')
 
     if request.method == 'POST':
-      email = request.form['email']
-      senha = request.form['senha']
-
-      resultado = validar_senha(senha)
-
-      if resultado is True:
         return redirect('/')
-
-      else:
-        return render_template('login.html', erros=resultado, email=email)
 
 app.run(host='0.0.0.0', port=8000)
